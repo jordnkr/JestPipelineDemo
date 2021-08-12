5e34b2e18608d5fc90da16f9062651263ce1ec6b
jordnkr <jordnkr408@gmail.com>
2021-08-12 12:43:22 -0500

    adding new test
    adding test

--- subtraction.test.js
+++ subtraction.test.js
@@ -2,3 +2,7 @@ const subtraction = require('./subtraction');
 test('subtracts 4 - 2 to equal 2', () => {
    expect(subtraction(4, 2)).toBe(2);
 });
+
+test('subtracts 5 - 1 to equal 4', () => {
+    expect(subtraction(5, 1)).toBe(4);
+ });
